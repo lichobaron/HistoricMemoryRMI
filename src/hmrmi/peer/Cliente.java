d182e4c87a5be7a3c9d8751ebc2037e998d058d0
kokoloko4 <anfecoquin@hotmail.com>
2018-11-17 18:53:15 -0500

    MatchFiles

--- src/hmrmi/peer/Cliente.java
+++ src/hmrmi/peer/Cliente.java
@@ -2,6 +2,8 @@ package hmrmi.peer;
 
 import java.rmi.*;
 import java.rmi.server.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Cliente extends Thread{
     private String ip;
@@ -15,6 +17,25 @@ public class Cliente extends Thread{
     public void run(){
         if (System.getSecurityManager() == null) {
             System.setSecurityManager(new RMISecurityManager());
+            String file = "a.txt";
+            List<String> urls = new ArrayList<String>();
+            urls.add("rmi://127.0.0.1:80/aaa/a.txt");
+            urls.add("rmi://127.0.0.1:80/bbb/a.txt");
+            urls.add("rmi://127.0.0.1:80/ccc/b.txt");
+            urls.add("rmi://127.0.0.1:80/ddd/a.txt");
+            matchFiles(urls, file);
         }
+    }
+
+    private List<String> matchFiles (List<String> urls, String file) {
+        List<String> matches = new ArrayList<String>();
+        for (String url : urls) {
+            String[] div = url.split("/");
+            if (div[div.length - 1].equals(file)){
+                matches.add(url);
+                System.out.println(div[div.length - 1]);
+            }
+        }
+        return matches;
     }
 }
