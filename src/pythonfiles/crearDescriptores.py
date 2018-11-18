8c41d59de161a1ebade487ca5807677881ffb0c8
kokoloko4 <anfecoquin@hotmail.com>
2018-11-17 20:51:47 -0500

    ArchivosPruebaDescriptors

---
+++ src/pythonfiles/crearDescriptores.py
@@ -0,0 +1,11 @@
+from random import randint
+for x in range (0,60):
+    f = open ('../../sources/descriptores/descriptor'+str(x+1)+'.txt','w')
+    for y in range (0,9):
+        if y == 0 :
+            f.write('test'+str(x+1)+'.txt'+'\n')
+        elif y == 8:
+            f.write('prueba'+str(randint(1,36))+'-'+str(randint(1,8)))
+        else:
+            f.write('prueba'+str(randint(1,36))+'-'+str(randint(1,8))+'\n')
+    f.close
